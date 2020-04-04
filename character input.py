ab8c2a178ecc24271a76fb96ead4c382f4ccb5f3
ldizon8 <lawdizon8@gmail.com>
2020-04-04 11:01:17 -0700

    Add files via upload

---
+++ character input.py
@@ -0,0 +1,20 @@
+'''Character Input Exercise
+
+Create a program that asks the user to enter their name and age. Print out the message that would tell them the year when they turn 100 years old.
+
+'''
+import datetime
+
+name = input("Hi, please enter your name: ")
+
+age = int(input("Please enter your age: "))
+
+today = datetime.datetime.now()
+
+
+#Bear in mind, the current year is 2020.
+print("Hi " + name + ", you will be 100 in the year " + str((int(today.year) - age) + 100) + ".")
+
+
+
+
